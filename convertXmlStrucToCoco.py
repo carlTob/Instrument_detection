7e4f1661482f1862f7a02b35ac74bfd3c4f9fce1
carlTob <carlsson_tobias@outlook.com>
2022-01-28 11:35:42 +0100

    changesTo script

--- convertXmlStrucToCoco.py
+++ convertXmlStrucToCoco.py
@@ -18,48 +18,53 @@ def XML2JSON(xml_path,out_path):
     xmlFiles=glob.glob(os.path.join(xml_path, '*.xml'))
     attrDict = dict()
 
-    attrDict["categories"]=[{"supercategory":"none","id":0,"name":"diatermi"},
-                    {"supercategory":"none","id":1,"name":"forceps"},
-                    {"supercategory":"none","id":2,"name":"head"},
-                    {"supercategory":"none","id":3,"name":"scalpel"},
-                {"supercategory":"none","id":4,"name":"needle driver"},
-                {"supercategory":"none","id":5,"name":"retractor"},
-                {"supercategory":"none","id":6,"name":"hl tube"},
-                {"supercategory":"none","id":7,"name":"saw"}
+    attrDict["categories"]=[{"id":0,"name":"diatermi","supercategory":"none"},
+                    {"id":1,"name":"forceps","supercategory":"none",},
+                    {"id":2,"name":"head","supercategory":"none",},
+                    {"id":3,"name":"scalpel","supercategory":"none",},
+                {"id":4,"name":"needle driver","supercategory":"none"},
+                {"id":5,"name":"retractor","supercategory":"none"},
+                {"id":6,"name":"hl tube","supercategory":"none"},
+                {"id":7,"name":"saw","supercategory":"none"}
                   ]
     images = list()
     annotations = list()
     image_id = 0
+    id1 = 1
     for file in xmlFiles:    
         image_id = image_id + 1      
         annotation_path=file
         image = dict()
         doc = xmltodict.parse(open(annotation_path).read(), force_list=('object'))
+        image['id'] = image_id
         image['file_name'] = str(doc['annotation']['filename'])
         image['height'] = int(doc['annotation']['size']['height'])
         image['width'] = int(doc['annotation']['size']['width'])
-        image['id'] = image_id
+        
         print ("File Name: {} and image_id {}".format(file, image_id))
         images.append(image)
-        id1 = 1
+        
         if 'object' in doc['annotation']:
             for obj in doc['annotation']['object']:
                 for value in attrDict["categories"]:
                     annotation = dict()          
                     if str(obj['name']) == value["name"]:
-                        annotation["iscrowd"] = 0
+                        annotation["id"] = id1
+
                         annotation["image_id"] = image_id
+
                         x1 = int(obj["bndbox"]["xmin"])  - 1
                         y1 = int(obj["bndbox"]["ymin"]) - 1
                         x2 = int(obj["bndbox"]["xmax"]) - x1
                         y2 = int(obj["bndbox"]["ymax"]) - y1                         
                         annotation["bbox"] = [x1, y1, x2, y2]
+                        annotation["segmentation"] = [[x1,y1,x1,(y1 + y2), (x1 + x2), (y1 + y2), (x1 + x2), y1]]
+
                         annotation["area"] = float(x2 * y2)
                         annotation["category_id"] = value["id"]
-                        annotation["ignore"] = 0
-                        annotation["id"] = id1
-                        annotation["segmentation"] = [[x1,y1,x1,(y1 + y2), (x1 + x2), (y1 + y2), (x1 + x2), y1]]
                         id1 +=1
+                        annotation["iscrowd"] = 0
+
                         annotations.append(annotation)
 
         else:
